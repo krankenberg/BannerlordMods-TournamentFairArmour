2ed0a7c84dbec2e9103df6bf117a6c0807229bbc
Skosnowich <skosnowich@googlemail.com>
2020-04-06 00:19:14 +0200

    Initial commit

---
+++ TournamentFairArmour/TournamentFairArmourMissionListener.cs
@@ -0,0 +1,52 @@
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace TournamentFairArmour
+{
+    public class TournamentFairArmourMissionListener : IMissionListener
+    {
+        private readonly Equipment _tournamentEquipment;
+
+        public TournamentFairArmourMissionListener(Equipment tournamentEquipment)
+        {
+            _tournamentEquipment = tournamentEquipment;
+        }
+
+        public void OnEquipItemsFromSpawnEquipmentBegin(Agent agent, Agent.CreationType creationType)
+        {
+            if (agent.IsHuman)
+            {
+                foreach (var overriddenEquipmentIndex in TournamentFairArmourSubModule.OverriddenEquipmentIndices)
+                {
+                    agent.SpawnEquipment[overriddenEquipmentIndex] = _tournamentEquipment[overriddenEquipmentIndex];
+                }
+
+                agent.AgentDrivenProperties.ArmorEncumbrance = agent.SpawnEquipment.GetTotalWeightOfArmor(agent.IsHuman);
+                agent.AgentDrivenProperties.ArmorHead = agent.SpawnEquipment.GetHeadArmorSum();
+                agent.AgentDrivenProperties.ArmorTorso = agent.SpawnEquipment.GetHumanBodyArmorSum();
+                agent.AgentDrivenProperties.ArmorLegs = agent.SpawnEquipment.GetLegArmorSum();
+                agent.AgentDrivenProperties.ArmorArms = agent.SpawnEquipment.GetArmArmorSum();
+            }
+        }
+
+        public void OnEquipItemsFromSpawnEquipment(Agent agent, Agent.CreationType creationType)
+        {
+        }
+
+        public void OnEndMission()
+        {
+        }
+
+        public void OnMissionModeChange(MissionMode oldMissionMode, bool atStart)
+        {
+        }
+
+        public void OnConversationCharacterChanged()
+        {
+        }
+
+        public void OnResetMission()
+        {
+        }
+    }
+}
